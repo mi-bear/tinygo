ad09a625cc148d24713836c05586e9d7bd35ebf5
Ayke van Laethem <aykevanlaethem@gmail.com>
2025-03-20 14:41:52 +0100

    builder: build wasi-libc inside TinyGo
    Instead of relying on a build when TinyGo is being built, do it like all
    other libraries when it is needed.
    
    This brings a few benefits:
    
      * No more running `make wasi-libc` on the command line as a special
        case for WebAssembly. The generic `git submodule update --init` step
        is now enough for wasi-libc.
      * It becomes much easier to customize the build per system. For
        example: include/exclude malloc as needed, disable/enable bulk
        memory operations per target, etc.

--- builder/library.go
+++ builder/library.go
@@ -25,6 +25,9 @@ type Library struct {
 	// cflags returns the C flags specific to this library
 	cflags func(target, headerPath string) []string
 
+	// cflagsForFile returns additional C flags for a particular source file.
+	cflagsForFile func(path string) []string
+
 	// The source directory.
 	sourceDir func() string
 
@@ -227,6 +230,7 @@ func (l *Library) load(config *compileopts.Config, tmpdir string, needLibc bool)
 	}
 	for _, path := range paths {
 		// Strip leading "../" parts off the path.
+		path := path
 		cleanpath := path
 		for strings.HasPrefix(cleanpath, "../") {
 			cleanpath = cleanpath[3:]
@@ -240,6 +244,9 @@ func (l *Library) load(config *compileopts.Config, tmpdir string, needLibc bool)
 			run: func(*compileJob) error {
 				var compileArgs []string
 				compileArgs = append(compileArgs, args...)
+				if l.cflagsForFile != nil {
+					compileArgs = append(compileArgs, l.cflagsForFile(path)...)
+				}
 				compileArgs = append(compileArgs, "-o", objpath, srcpath)
 				if config.Options.PrintCommands != nil {
 					config.Options.PrintCommands("clang", compileArgs...)
